e4ca7dea023e3fe183739325f4beefc4590bf367
yuanxizhang <yuanxi.zhang@gmail.com>
2019-01-29 17:44:22 -0800

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	configure do
+	  enable :sessions
+	  set :session_secret, "terces987"
+	end
+
+	get '/' do 
+		erb :index
+	end	
+
+	post '/checkout' do 
+		@session = session
+		@session[:item] = params[:item]
+		erb :items 
+	end
 end
